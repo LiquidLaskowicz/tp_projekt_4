edec22535a1901336b307280389837b2c498c073
LiquidLaskowicz <mixerak123@gmail.com>
2024-06-02 17:50:10 +0200

    zmiana kształtu, przesunięcie na środek

--- PlanarQuadrotor/planar_quadrotor_visualizer.cpp
+++ PlanarQuadrotor/planar_quadrotor_visualizer.cpp
@@ -1,5 +1,6 @@
 #include "planar_quadrotor_visualizer.h"
 
+
 PlanarQuadrotorVisualizer::PlanarQuadrotorVisualizer(PlanarQuadrotor *quadrotor_ptr): quadrotor_ptr(quadrotor_ptr) {}
 
 /**
@@ -13,10 +14,25 @@ void PlanarQuadrotorVisualizer::render(std::shared_ptr<SDL_Renderer> &gRenderer)
     float q_x, q_y, q_theta;
 
     /* x, y, theta coordinates */
-    q_x = state[0];
-    q_y = state[1];
+    q_x = state[0] + 640; // <---* D
+    q_y = state[1] + 360; // <---* D
     q_theta = state[2];
 
-    SDL_SetRenderDrawColor(gRenderer.get(), 0xFF, 0x00, 0x00, 0xFF);
-    filledCircleColor(gRenderer.get(), q_x, q_y, 30, 0xFF0000FF);
+    SDL_SetRenderDrawColor(gRenderer.get(), 0xFF, 0xC8, 0x00, 0xFF);
+
+    // <---* D
+    SDL_Rect bodyRect;
+    bodyRect.x = q_x - 50; 
+    bodyRect.y = q_y - 5;  
+    bodyRect.w = 100;      
+    bodyRect.h = 10;
+    // <---* D
+    // <---* D
+    SDL_RenderFillRect(gRenderer.get(), &bodyRect);
+    SDL_SetRenderDrawColor(gRenderer.get(), 0x00, 0x00, 0x00, 0xFF);
+    SDL_RenderDrawLine(gRenderer.get(), q_x + 38, q_y -15, q_x + 52, q_y - 15);
+    SDL_RenderDrawLine(gRenderer.get(), q_x + 45, q_y - 15, q_x + 45, q_y - 5);
+    SDL_RenderDrawLine(gRenderer.get(), q_x - 38, q_y - 15, q_x - 52, q_y - 15);
+    SDL_RenderDrawLine(gRenderer.get(), q_x - 45, q_y - 15, q_x - 45, q_y - 5);
+    // <---* D
 }
